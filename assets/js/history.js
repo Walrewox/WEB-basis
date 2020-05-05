22e98ad8463ce737a32e4bb1d35e6b4eef910759
Olexandr <saske.pawlyuk2012@gmail.com>
2020-05-05 09:18:55 +0300

    Clean code

--- assets/js/history.js
+++ assets/js/history.js
@@ -1,45 +1,27 @@
+function content(url,button){
+  $.ajax({
+    url: url,
+    success: function(html){
+      $('#info').html(html);
+    }
+  });
+  $("*").removeClass('choice-active');
+  $(button).addClass("choice-active");
+}
 window.addEventListener('popstate', (event) => {
   console.log("location: " + document.location + ", state: " + event.state);
   switch (event.state) {
     case 'author':
-      $.ajax({
-        url: 'ajax-content/author.html',
-        success: function(html){
-          $('#info').html(html);
-        }
-      });
-      $("*").removeClass('choice-active');
-      $('#author').addClass("choice-active");
+      content('ajax-content/author.html','#author');
       break;
     case 'biography':
-      $.ajax({
-        url: 'ajax-content/bio.php',
-        success: function(html){
-          $('#info').html(html);
-        }
-      });
-      $("*").removeClass('choice-active');
-      $('#bio').addClass("choice-active");
+      content('ajax-content/bio.php','#bio')
       break;
     case 'education':
-      $.ajax({
-        url: 'ajax-content/edu.html',
-        success: function(html){
-          $('#info').html(html);
-        }
-      });
-      $("*").removeClass('choice-active');
-      $('#edu').addClass("choice-active");
+      content('ajax-content/edu.html','#edu');
       break;
     case 'hobby':
-      $.ajax({
-        url: 'ajax-content/hobby.html',
-        success: function(html){
-          $('#info').html(html);
-        }
-      });
-      $("*").removeClass('choice-active');
-      $('#hobby').addClass("choice-active");
+      content('ajax-content/hobby.html','#hobby');
       break;
   }
 });
