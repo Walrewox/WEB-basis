26d2895a12916077712d8670fcecd4617b98c600
Olexandr <saske.pawlyuk2012@gmail.com>
2020-05-14 09:08:38 +0300

    Anchor links for navigation on page

--- assets/js/main_content.js
+++ assets/js/main_content.js
@@ -1,50 +1,71 @@
+let linkButtons = Array.from(document.getElementByClassname('choice'));
+function selectButton(id){
+  linkButtons.forEach()
+}
+
 $(document).ready(function(){
-  $('#author').click(function(){
-    $.ajax({
-      url: 'ajax-content/index/author.html',
-      success: function(html){
-        $('#info').html(html);
-      }
-    });
-    $("*").removeClass('choice-active');
-    $(this).addClass("choice-active");
-    
-    window.history.pushState('author',null,'author');
+  $('nav li a').click(function(e) {
+             e.preventDefault();
+
+             var href = $(this).attr("href");
+
+              $.ajax({
+                url: href,
+                type: GET,
+                success: function(data){
+                    wrapper.html(data);
+                }
+              });
+
+            history.pushState('', 'New URL: '+href, href);
+
   });
-  $('#bio').click(function(){
-    $.ajax({
-      url: 'ajax-content/index/bio.php',
-      success: function(html){
-        $('#info').html(html);
-      }
-    });
-    $("*").removeClass('choice-active');
-    $(this).addClass("choice-active");
-    
-    window.history.pushState('bio',null,'bio');
   });
-  $('#edu').click(function(){
-    $.ajax({
-      url: 'ajax-content/index/edu.html',
-      success: function(html){
-        $('#info').html(html);
-      }
-    });
-    $("*").removeClass('choice-active');
-    $(this).addClass("choice-active");
-    
-    window.history.pushState('edu',null,'edu');
-  });
-  $('#hobby').click(function(){
-    $.ajax({
-      url: 'ajax-content/index/hobby.html',
-      success: function(html){
-        $('#info').html(html);
-      }
-    });
-    $("*").removeClass('choice-active');
-    $(this).addClass("choice-active");
-    
-    window.history.pushState('hobby',null,'hobby');
-  });
-}); 
+  // $('#author').click(function(){
+  //   $.ajax({
+  //     url: 'ajax-content/index/author.html',
+  //     success: function(html){
+  //       $('#info').html(html);
+  //     }
+  //   });
+  //   $("*").removeClass('choice-active');
+  //   $(this).addClass("choice-active");
+  //
+  //   window.history.pushState({content:author},null,'/index/author');
+  // });
+  // $('#bio').click(function(){
+  //   $.ajax({
+  //     url: 'ajax-content/index/bio.php',
+  //     success: function(html){
+  //       $('#info').html(html);
+  //     }
+  //   });
+  //   $("*").removeClass('choice-active');
+  //   $(this).addClass("choice-active");
+  //
+  //   window.history.pushState({content:bio},null,'/index/bio');
+  // });
+  // $('#edu').click(function(){
+  //   $.ajax({
+  //     url: 'ajax-content/index/edu.html',
+  //     success: function(html){
+  //       $('#info').html(html);
+  //     }
+  //   });
+  //   $("*").removeClass('choice-active');
+  //   $(this).addClass("choice-active");
+  //
+  //   window.history.pushState({content:edu},null,'/index/edu');
+  // });
+  // $('#hobby').click(function(){
+  //   $.ajax({
+  //     url: 'ajax-content/index/hobby.html',
+  //     success: function(html){
+  //       $('#info').html(html);
+  //     }
+  //   });
+  //   $("*").removeClass('choice-active');
+  //   $(this).addClass("choice-active");
+  //
+  //   window.history.pushState({content:hobby},null,'/index/hobby');
+  // });
--- index.php
+++ index.php
@@ -1,11 +1,41 @@
-<?php 
+<?php
   $title = "Головна";
-  require('requires/head.php');?>
+  require('requires/head.php');
+  $navigation = [[
+                    'href' => '/author/',
+                    'title'=> 'Автор',
+                    'id' => 'author'
+                  ],
+                  [
+                    'href' => '/bio/',
+                    'title'=> 'Надихаючі особистості',
+                    'id' => 'bio'
+                  ],
+                  [
+                    'href' =>'/edu/',
+                    'title'=>'Освіта',
+                    'id' => 'edu'
+                  ],
+                  [
+                    'href' =>'/hobby/',
+                    'title'=>'Хобі',
+                    'id' => 'hobby'
+                  ]
+                  ];
+?>
       <div class="choices_block">
         <div class="choice">
-          <h1>Головна сторінка</h1>  
+          <h1>Головна сторінка</h1>
         </div>
-        <div class="choice" id="author">
+        <nav>
+          <?php foreach ($navigation as $link) {
+              echo "<div class = \"choice\" id=\"{$link['id']}\">";
+              echo "<li><a href=\"{$link['href']}\"> {$link['title']} </a></li>\n";
+              echo "</div>";
+              }
+          ?>
+        </nav>
+        <!-- <div class="choice" id="author">
           <input type="button" id="author" value="Автор">
         </div>
         <div class="choice" id="bio">
@@ -16,7 +46,7 @@
         </div>
         <div class="choice" id="hobby">
           <input type="button" id="hobby" value="Хобі">
-        </div>
+        </div> -->
       </div>
       <div class="info_block" id="info">
         <h1 class="T-center">Натисніть на одну з кнопок зліва!</h1>
